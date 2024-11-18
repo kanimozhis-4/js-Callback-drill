01bc316e753cedabd91cd6a6c1739f314764e824
kanimozhis-4 <kanimozhi808088@gmail.com>
2024-11-18 15:37:22 +0530

    added promise drill

---
+++ Promise/Test/2-TestProblem.js
@@ -0,0 +1,12 @@
+import {readAndConvertToUpperCase,convertToLowerCaseAndSplit,sortAndWriteSentence,cleanupGeneratedFiles} from '../2-problem.js'
+const allFileName = 'filenames.txt'; 
+readAndConvertToUpperCase('../lipsum.txt', './uppercase.txt', allFileName)
+.then(()=>{
+    return convertToLowerCaseAndSplit('./uppercase.txt', './lowercase.txt', allFileName)
+})
+.then(()=>{
+    return sortAndWriteSentence(`./lowercase.txt`, `./sorted.txt`,allFileName)
+}) 
+.then(()=>{
+    return cleanupGeneratedFiles(allFileName)
+})
